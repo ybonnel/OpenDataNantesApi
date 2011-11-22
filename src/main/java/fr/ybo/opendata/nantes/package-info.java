7986cb5ff8008a213e7071cb56b580dae7037271
ybonnel <ybonnel@sopragroup.com>
2011-11-22 16:57:30 +0100

    Ajout de doc sur le package principal.

---
+++ src/main/java/fr/ybo/opendata/nantes/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * API permettant d'accèder aux données de data.nantes.fr.
+ * Utilisation de l'api :
+ * <ul>
+ *     <li>Création de l'instance :<br/>
+ *     {@code OpenDataApi openDataApi = new OpenDateApi("key");}.</li>
+ *     <li>Utilisation :<br/>
+ *     {@code openDataApi.getParkings()}.</li>
+ * </ul>
+ */
+package fr.ybo.opendata.nantes;
